c49ab39e5900b1a69772eaad9248da10718fe56b
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-10-10 08:34:22 -0700

    add utils

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
